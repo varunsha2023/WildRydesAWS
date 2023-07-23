82116ceeb2ab205c38644a63ee3f41cc543becfa
Varun <shrivastavvaruncha@gmail.com>
2023-07-24 00:41:40 +0530

    adding file

---
+++ js/config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'ap-south-1_xP1vCAsIa', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '29orf00jtp2a622c5179udoh5i', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'ap-south-1' // e.g. us-east-2
+    },
+    api: {
+        invokeUrl: 'https://ijmhqqooh9.execute-api.ap-south-1.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+    }
+};
